b69ee7d89b6cf73237fcf7c8e624ec242a537947
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-17 02:30:08 +0300

    var element from html with JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var productNameInput = document.getElementById('pName');
+var productPriceInput = document.getElementById('pPrice');
+var productCategoryInput = document.getElementById('pCategory');
+var productImageInput = document.getElementById('pImage');
+var productDescInput = document.getElementById('pDesc');
+
+
+console.log(productNameInput)
+console.log(productPriceInput)
+console.log(productCategoryInput)
+console.log(productImageInput)
+console.log(productDescInput)
