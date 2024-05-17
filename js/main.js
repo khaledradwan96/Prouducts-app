e1725f13e530c4cb51862a3eec92b1f961f0c2ae
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-17 15:33:36 +0300

    try array of object

--- js/main.js
+++ js/main.js
@@ -27,3 +27,21 @@ function clearForm(){
     productImageInput.value = null;
     productDescInput.value = null;    
 }
+
+
+// json
+var newArr = [
+    {name: "nokia", price: "2000", category: "Tv", image: "honoe.webp", desc: "asss"},
+    {name: "phone", price: "2000", category: "Tv", image: "honoe.webp", desc: "asss"},
+    {name: "Oppo", price: "2000", category: "Tv", image: "honoe.webp", desc: "asss"},
+    {name: "iphone", price: "2000", category: "Tv", image: "honoe.webp", desc: "asss"},
+
+]
+
+var box = ''
+for(var i=0; i<newArr.length; i++){
+    box += `
+    <h2>${newArr[i].name}</h2>
+    <p>${newArr[i].price}</p>`
+}
+console.log(box)
