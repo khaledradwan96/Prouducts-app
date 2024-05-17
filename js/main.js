ba4f9a16f0fc4ac002b9afd07606eb6e28bc1a48
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-17 17:45:08 +0300

    code function clearAll() for delete all products

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ if(localStorage.getItem('products') === null){  // user is new
     displayProduct();
     }            
 
+
 function addProduct(){
    var product = {
     name : productNameInput.value,
@@ -25,9 +26,6 @@ function addProduct(){
    productContainer.push(product);
    localStorage.setItem('products', JSON.stringify(productContainer))
    displayProduct()
-
-   
-
 }
 
 function clearForm(){
@@ -42,19 +40,24 @@ function displayProduct(){
     var cartona = "";
 
 for(var i=0; i<productContainer.length; i++){
-cartona += `<div class="col-md-4">
+cartona += `<div class="col-md-4" id="product${i+1}">
 <div class="product border border-2 shadow p-3 rounded-3">
   <img src="images/phone1.webp" class="w-100" alt="phone">
-  <h2 class="h4 fw-bold">${productContainer[i].name}</h2>
+  <h2 class="h4 fw-bold mt-3">${productContainer[i].name}</h2>
   <p class="text-secondary">${productContainer[i].desc}</p>
-  <h3 class="h5"><span class="fw-bold">Price :</span>${productContainer[i].price}</h3>
-  <h3 class="h5"><span class="fw-bold">Category :</span>${productContainer[i].category}</h3>
+  <h3 class="h5"><span class="fw-bold">Price: </span>${productContainer[i].price}</h3>
+  <h3 class="h5"><span class="fw-bold">Category: </span>${productContainer[i].category}</h3>
   <button class="btn btn-outline-danger mb-2"> <i class="fa-solid fa-delete-left"></i> Delete</button>
   <button class="btn btn-outline-warning mb-2"> <i class="fa-solid fa-edit"></i> Update</button>
 </div>
 </div>`
     
 }
-console.log(cartona)
 document.getElementById("myRow").innerHTML = cartona
+}
+
+function clearAll(){
+    localStorage.clear();
+    productContainer = [];
+    document.getElementById("myRow").innerHTML = ""
 }
