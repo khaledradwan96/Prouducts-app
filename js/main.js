6e365d4b7274397b6dc8d89b9574db049caf39e7
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-17 16:24:40 +0300

    make function displayProduct() get data added

--- js/main.js
+++ js/main.js
@@ -34,14 +34,13 @@ function displayProduct(){
     var cartona = "";
 
 for(var i=0; i<productContainer.length; i++){
-cartona += `<div class="col-md-3">
+cartona += `<div class="col-md-4">
 <div class="product border border-2 shadow p-3 rounded-3">
   <img src="images/phone1.webp" class="w-100" alt="phone">
-  <h2 class="h4 fw-bold">Nokia</h2>
-  <p class="text-secondary">Lorem, ipsum dolor.</p>
-  <h3 class="h5"><span class="fw-bold">Price :</span>5000</h3>
-  <h3 class="h5"><span class="fw-bold">Category :</span>mobile</h3>
-  <h3 class="h5 mb-3"><span class="fw-bold">Price :</span>5000</h3>
+  <h2 class="h4 fw-bold">${productContainer[i].name}</h2>
+  <p class="text-secondary">${productContainer[i].desc}</p>
+  <h3 class="h5"><span class="fw-bold">Price :</span>${productContainer[i].price}</h3>
+  <h3 class="h5"><span class="fw-bold">Category :</span>${productContainer[i].category}</h3>
   <button class="btn btn-outline-danger"> <i class="fa-solid fa-delete-left"></i>Delete</button>
   <button class="btn btn-outline-warning"> <i class="fa-solid fa-edit"></i>Update</button>
 </div>
