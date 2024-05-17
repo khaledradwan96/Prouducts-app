994eb33bec4167e2150cb32f063620ffeb063e63
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-17 16:15:20 +0300

    code function displayProduct()

--- js/main.js
+++ js/main.js
@@ -14,10 +14,12 @@ function addProduct(){
     desc: productDescInput.value
    };
 
+   clearForm()
    productContainer.push(product);
+   displayProduct()
    console.log(productContainer)
 
-   clearForm()
+
 }
 
 function clearForm(){
@@ -26,6 +28,28 @@ function clearForm(){
     productCategoryInput.value = null;
     productImageInput.value = null;
     productDescInput.value = null;    
+}
+
+function displayProduct(){
+    var cartona = "";
+
+for(var i=0; i<productContainer.length; i++){
+cartona += `<div class="col-md-3">
+<div class="product border border-2 shadow p-3 rounded-3">
+  <img src="images/phone1.webp" class="w-100" alt="phone">
+  <h2 class="h4 fw-bold">Nokia</h2>
+  <p class="text-secondary">Lorem, ipsum dolor.</p>
+  <h3 class="h5"><span class="fw-bold">Price :</span>5000</h3>
+  <h3 class="h5"><span class="fw-bold">Category :</span>mobile</h3>
+  <h3 class="h5 mb-3"><span class="fw-bold">Price :</span>5000</h3>
+  <button class="btn btn-outline-danger"> <i class="fa-solid fa-delete-left"></i>Delete</button>
+  <button class="btn btn-outline-warning"> <i class="fa-solid fa-edit"></i>Update</button>
+</div>
+</div>`
+    
+}
+console.log(cartona)
+document.getElementById("myRow").innerHTML = cartona
 }
 
 
