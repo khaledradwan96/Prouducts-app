45ad84d41c15b1fc4c4a437008d12d12d18088b3
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-22 23:33:28 +0300

    code function deleteProduct()

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ var productDescInput = document.getElementById('pDesc');
 var productContainer;
 if(localStorage.getItem('products') === null){  // user is new
     productContainer = []
-    }else{ // user have storage data
+    }
+    else{ // user have storage data
     productContainer = JSON.parse(localStorage.getItem('products'));
     displayProduct();
     }            
@@ -38,20 +39,21 @@ function clearForm(){
 
 function displayProduct(){
     var cartona = "";
-
 for(var i=0; i<productContainer.length; i++){
-cartona += `<div class="col-md-4" id="product${i+1}">
-<div class="product border border-2 shadow p-3 rounded-3">
-  <img src="images/phone1.webp" class="w-100" alt="phone">
-  <h2 class="h4 fw-bold mt-3">${productContainer[i].name}</h2>
-  <p class="text-secondary">${productContainer[i].desc}</p>
-  <h3 class="h5"><span class="fw-bold">Price: </span>${productContainer[i].price}</h3>
-  <h3 class="h5"><span class="fw-bold">Category: </span>${productContainer[i].category}</h3>
-  <button class="btn btn-outline-danger mb-2"> <i class="fa-solid fa-delete-left"></i> Delete</button>
-  <button class="btn btn-outline-warning mb-2"> <i class="fa-solid fa-edit"></i> Update</button>
-</div>
+cartona += `
+<div class="col-md-4" id="product${i+1}">
+    <div class="product border border-2 shadow p-3 rounded-3">
+        <img src="images/phone1.webp" class="w-100" alt="phone">
+        <h2 class="h4 fw-bold mt-3">${productContainer[i].name}</h2>
+        <p class="text-secondary">${productContainer[i].desc}</p>
+        <h3 class="h5"><span class="fw-bold">Price: </span>${productContainer[i].price}</h3>
+        <h3 class="h5"><span class="fw-bold">Category: </span>${productContainer[i].category}</h3>
+        <button onclick="deleteProduct(${i})" class="btn btn-outline-danger mb-2"> 
+            <i class="fa-solid fa-delete-left"></i> Delete</button>
+        <button class="btn btn-outline-warning mb-2">
+            <i class="fa-solid fa-edit"></i> Update</button>
+    </div>
 </div>`
-    
 }
 document.getElementById("myRow").innerHTML = cartona
 }
@@ -60,4 +62,12 @@ function clearAll(){
     localStorage.clear();
     productContainer = [];
     document.getElementById("myRow").innerHTML = ""
+}
+
+function deleteProduct(index){
+    console.log(index) // for get index of click
+    productContainer.splice(index, 1) // for delete the index
+    localStorage.setItem('products', JSON.stringify(productContainer)) 
+    // to change localStorage
+    displayProduct() // to display product in the page
 }
