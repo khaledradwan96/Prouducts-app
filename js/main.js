70645d985b2716fb291598002d2dad96f4a9bc7c
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-24 00:12:46 +0300

    edit functions displayProduct(arr) +  and add style product:hover

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ if(localStorage.getItem('products') === null){  // user is new
     productContainer = []
     }else{ // user have storage data
     productContainer = JSON.parse(localStorage.getItem('products'));
-    displayProduct();
+    displayProduct(productContainer);
     };           
 
 
@@ -30,7 +30,7 @@ function addProduct(){
    clearForm()
    productContainer.push(product);
    localStorage.setItem('products', JSON.stringify(productContainer))
-   displayProduct()
+   displayProduct(productContainer)
 };
 
 function clearForm(){
@@ -41,17 +41,17 @@ function clearForm(){
     productDescInput.value = null;    
 }
 
-function displayProduct(){
+function displayProduct(arr){
     var cartona = "";
-for(var i=0; i<productContainer.length; i++){
+for(var i=0; i<arr.length; i++){
 cartona += `
-<div class="col-md-4" id="product${i+1}">
+<div class="col-md-4"">
     <div class="product border border-2 shadow p-3 rounded-3">
-        <img src="${productContainer[i].image}" class="w-100" alt="">
-        <h2 class="h4 fw-bold mt-3">${productContainer[i].name}</h2>
-        <p class="text-secondary">${productContainer[i].desc}</p>
-        <h3 class="h5"><span class="fw-bold">Price: </span>${productContainer[i].price}</h3>
-        <h3 class="h5"><span class="fw-bold">Category: </span>${productContainer[i].category}</h3>
+        <img src="${arr[i].image}" class="w-100" alt="">
+        <h2 class="h4 fw-bold mt-3">${arr[i].name}</h2>
+        <p class="text-secondary">${arr[i].desc}</p>
+        <h3 class="h5"><span class="fw-bold">Price: </span>${arr[i].price}</h3>
+        <h3 class="h5"><span class="fw-bold">Category: </span>${arr[i].category}</h3>
         <button onclick="deleteProduct(${i})" class="btn btn-outline-danger mb-2"> 
             <i class="fa-solid fa-delete-left"></i> Delete</button>
         <button  onclick="getValues(${i})" class="btn btn-outline-warning mb-2">
@@ -73,16 +73,17 @@ function deleteProduct(index){
     productContainer.splice(index, 1) // for delete the index
     localStorage.setItem('products', JSON.stringify(productContainer)) 
     // to change localStorage
-    displayProduct() // to display product in the page
+    displayProduct(productContainer) // to display product in the page
 };
 
 function search(){
     var term = searchInput.value;
-    var cartona = ""
+    var cartona = "";
+    var box=[];
     for(var i=0; i<productContainer.length; i++){
         if(productContainer[i].name.toLowerCase().includes(term.toLowerCase())){
             cartona += `
-            <div class="col-md-4" id="product${i+1}">
+            <div class="col-md-4"">
                 <div class="product border border-2 shadow p-3 rounded-3">
                     <img src="${productContainer[i].image}" class="w-100" alt="">
                     <h2 class="h4 fw-bold mt-3">${productContainer[i].name.replace(term, '<span class="bg-warning">'+term+'</span>')}</h2>
@@ -123,7 +124,7 @@ function updateProduct(updateIndex){
     // productImageInput[updateIndex].image = productImageInput.value
     productContainer[updateIndex].desc = productDescInput.value
 
-    displayProduct()
+    displayProduct(productContainer);
     localStorage.setItem('products', JSON.stringify(productContainer))
     clearForm()
 
